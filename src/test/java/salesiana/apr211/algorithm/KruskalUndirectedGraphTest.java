6775a9998408e400c3dab277a2e55bc2f1dc98fd
MorveN11 <morales.patty.jose@gmail.com>
2023-03-13 21:25:31 -0400

    fix: add necesary imports

--- src/test/java/salesiana/apr211/algorithm/KruskalUndirectedGraphTest.java
+++ src/test/java/salesiana/apr211/algorithm/KruskalUndirectedGraphTest.java
@@ -1,6 +1,7 @@
 package salesiana.apr211.algorithm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Set;
 import java.util.TreeSet;

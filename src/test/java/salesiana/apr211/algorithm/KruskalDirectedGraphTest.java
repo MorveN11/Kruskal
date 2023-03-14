01ba5e4b6cc9df54ed7d33fce8b5ebd0ca3a51f2
MorveN11 <morales.patty.jose@gmail.com>
2023-03-13 22:05:53 -0400

    feat: Add New Tests

--- src/test/java/salesiana/apr211/algorithm/KruskalDirectedGraphTest.java
+++ src/test/java/salesiana/apr211/algorithm/KruskalDirectedGraphTest.java
@@ -1,26 +1,30 @@
 package salesiana.apr211.algorithm;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Set;
+import java.util.TreeSet;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import salesiana.apr211.graphs.DirectedGraph;
+import salesiana.apr211.graphs.Edge;
 import salesiana.apr211.graphs.Graph;
 import salesiana.apr211.graphs.Node;
 
 class KruskalDirectedGraphTest {
 
+  private static final Node<Integer> node1 = new Node<>(1);
+  private static final Node<Integer> node2 = new Node<>(2);
+  private static final Node<Integer> node3 = new Node<>(3);
+  private static final Node<Integer> node4 = new Node<>(4);
+  private static final Node<Integer> node5 = new Node<>(5);
+  private static final Node<Integer> node6 = new Node<>(6);
   private static Graph<Integer> graph;
 
   @BeforeEach
   void setup() {
     graph = new DirectedGraph<>();
-    final Node<Integer> node1 = new Node<>(1);
-    final Node<Integer> node2 = new Node<>(2);
-    final Node<Integer> node3 = new Node<>(3);
-    final Node<Integer> node4 = new Node<>(4);
-    final Node<Integer> node5 = new Node<>(5);
-    final Node<Integer> node6 = new Node<>(6);
     assertTrue(graph.addEdge(3, node1, node2));
     assertTrue(graph.addEdge(5, node1, node5));
     assertTrue(graph.addEdge(2, node5, node6));
@@ -33,7 +37,18 @@ class KruskalDirectedGraphTest {
 
   @Test
   void testKruskalAlgorithm() {
+    Set<Edge<Integer>> expectedGraph = new TreeSet<>();
+    expectedGraph.add(new Edge<>(2, node5, node6));
+    expectedGraph.add(new Edge<>(3, node1, node2));
+    expectedGraph.add(new Edge<>(3, node3, node6));
+    expectedGraph.add(new Edge<>(5, node1, node5));
+    expectedGraph.add(new Edge<>(7, node6, node4));
+    assertEquals(6, graph.getNumNodes());
+    assertEquals(8, graph.getNumEdges());
     Kruskal<Integer> kruskal = new Kruskal<>(graph);
-    System.out.println(kruskal);
+    assertEquals(6, kruskal.getGraph().getNumNodes());
+    assertEquals(5, kruskal.getGraph().getNumEdges());
+    assertEquals(20, kruskal.getMinimumCost());
+    assertEquals(expectedGraph, kruskal.getGraph().getAllEdges());
   }
 }
